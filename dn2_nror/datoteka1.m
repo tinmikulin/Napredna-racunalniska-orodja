8a6c6aca34ad7c358e09df6a6571fe1dd173cf07
tinmikulin <146851341+tinmikulin@users.noreply.github.com>
2023-11-13 11:23:41 +0100

    Add files via upload

---
+++ dn2_nror/datoteka1.m
@@ -0,0 +1,7 @@
+(* ::Package:: *)
+
+(* ::Input:: *)
+(*datoteka1[n_]:=Module[{tocke,znotrajTocke},tocke=RandomReal[{-1,1},{n,2}];*)
+(*znotrajTocke=Select[tocke,Norm[#]<=1&];*)
+(*4 Length[znotrajTocke]/Length[tocke]]*)
+(**)
